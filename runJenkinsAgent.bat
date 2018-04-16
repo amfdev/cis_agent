a5fe0543329307ad28db0fd9e5d81b801b83bf16
admin <admin@AMF-UBUNTU16-LC01.luxoft.com>
2018-04-17 00:46:13 +0300

    jn agent scripts

---
+++ runJenkinsAgent.bat
@@ -0,0 +1,9 @@
+c:
+cd c:\JN
+
+:up 
+
+rem replace this line with java -jar slave.jar -jnlpUrl ...
+
+echo reconnecting
+goto up
---
+++ runJenkinsAgent.sh
@@ -0,0 +1,22 @@
+SCRIPT=`realpath $0`
+SCRIPTPATH=`dirname $SCRIPT`
+
+export CIS_TOOLS=${SCRIPTPATH}
+JENKINS_ROOT=${SCRIPTPATH}/..
+
+source ./runJenkinsAgent.conf
+
+wget --timestamping  ${JENKINS_SERVER}/jnlpJars/agent.jar
+
+source scl_source enable devtoolset-7
+
+gcc --version
+
+while true
+do
+    echo "connecting..."
+    java -jar agent.jar -jnlpUrl ${JENKINS_SERVER}/computer/${JENKINS_AGENTNAME}/slave-agent.jnlp -secret ${JENKINS_SECRET} -workDir "${JENKINS_ROOT}"
+    sleep 5
+done
+
+
